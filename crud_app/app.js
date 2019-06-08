d68b7271b45eb8efe4aad645d94320b718b55178
banditek85 <mitjasusec@outlook.com>
2019-06-08 18:33:16 +0200

    updated openlayers

---
+++ crud_app/app.js
@@ -0,0 +1,10 @@
+function add(a, b) {
+  let result = a + b;
+  return result;
+}
+
+let con = add(10, 43);
+
+const tbody = document.getElementsByTagName("tbody")[0];
+let attr = tbody["id"];
+let prop = tbody.id;
