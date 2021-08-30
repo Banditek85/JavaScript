10c82ad904a17dba96c2b0bc3460d612a3cfc76c
mitjasusec@outlook.com <mitja.susec@codingmonkeys.net>
2021-08-30 20:38:01 +0200

    completed sorted list

--- Sortable_List/index.js
+++ Sortable_List/index.js
@@ -1,45 +1,43 @@
-function renderList(p_list) {
-    return p_list.map((el) => {
-        return `<li class="draggable dropzone" draggable="true" data-id = ${el.id}><p>${el.name}</p></li>`
-    }).join("")
+Array.prototype.swap = function (x, y) {
+    const temp = this[x];
+    this[x] = this[y];
+    this[y] = temp;
+}
+
+function render_list(p_list) {
+    let my_list = document.querySelector("#my_list");
+
+    my_list.innerHTML = p_list.map((el) => {
+        return `<li class="draggable dropzone" draggable="true" id=${el.id}><p>${el.name}</p></li>`
+    }).join("");
 };
 
-const list = [{ name: "Apples" },
-{ name: "Onions" },
-{ name: "Carrots" },
-{ name: "Plums" }];
+const list = [
+    { id: 1, name: "Apples" },
+    { id: 2, name: "Onions" },
+    { id: 3, name: "Carrots" },
+    { id: 4, name: "Plums" }
+];
 
-list.forEach((obj, i) => {
-    obj.id = i;
-})
-
-const my_list = document.querySelector("#my_list");
-my_list.innerHTML = renderList(list);
+render_list(list);
 
 my_list.addEventListener("dragstart", onDragStart); // on dragged element
-
 my_list.addEventListener("drop", onDragDrop); // on dropped target
-
 my_list.addEventListener("dragover", onDragOver); // on dropped target
-
 my_list.addEventListener("dragenter", onDragEnter);
-
 my_list.addEventListener("dragend", onDragEnd);
-
 my_list.addEventListener("dragleave", onDragLeave);
 
 function onDragStart(e) {
     if (e.target.classList.contains("draggable")) {
         e.target.classList.add("dragged");
-        e.dataTransfer.setData('text/plain', e.target.id);
+        e.dataTransfer.setData('text/id', e.target.id);
     }
 }
 
 function onDragEnd(e) {
     if (e.target.classList.contains("dragged")) {
         e.target.classList.remove("dragged");
-
-        console.log("Dragged element " + e.target.dataset.id)
     }
 }
 
@@ -48,7 +46,14 @@ function onDragDrop(e) {
     if (e.target.classList.contains("dropzone")) {
         e.target.classList.remove("drag_target");
 
-        console.log("Dropped on element " + e.target.dataset.id)
+        const dragged_id = parseInt(e.dataTransfer.getData("text/id"), 10);
+        let index1 = list.findIndex(item => item.id == dragged_id);
+        let index2 = list.findIndex(item => item.id == e.target.id);
+
+        // console.log(`Dropping element with id ${dragged_id} at index ${index1} to element with id ${e.target.id} at index ${index2}`);
+
+        list.swap(index1, index2);
+        render_list(list);
     }
 }
 
